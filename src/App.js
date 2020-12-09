6f6e45747a7f2b15bb5a089c5d28df095d301d52
Martastartgit <marta.petrivska@gmail.com>
2020-12-09 17:20:02 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,2 @@
-import React, {Component} from 'react';
-import AllPost from "./Components/All-post/AllPost";
 
-class App extends Component {
-  render() {
-    return (
-        <div>
-             <AllPost/>
-        </div>
-    );
-  }
-}
-
-export default App;
 
